9c7f38493cab3f4f4ed284c5db06502b5b92d943
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2021-02-05 17:54:17 +0100

    fix getStaticProps

--- pages/api/sensors/[id].ts
+++ pages/api/sensors/[id].ts
@@ -2,14 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 import random from 'lodash/random'
 
-const handler = nc<NextApiRequest, NextApiResponse>().get((req, res) => {
-  const { id } = req.query
-  const lat = random(-90, 90, true).toFixed(4)
-  const lon = random(-180, 180, true).toFixed(4)
-
+const handler = nc<NextApiRequest, NextApiResponse>().get(async (req, res) => {
   try {
-    // const weatherDataResponse = await fetch(`https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`)
-    // const json = await weatherDataResponse.json()
+    const { id } = req.query
+    const lat = random(-90, 90, true).toFixed(4)
+    const lon = random(-180, 180, true).toFixed(4)
+    const weatherDataResponse = await fetch(`https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`)
+    const json = await weatherDataResponse.json()
 
     res.json({
       data: {
@@ -18,24 +17,15 @@ const handler = nc<NextApiRequest, NextApiResponse>().get((req, res) => {
           lat,
           lon,
         },
+        weather: json,
       },
-      // weather: json,
     })
   } catch (err) {
-    console.log(err)
+    console.error(err)
 
     res.json({
       data: null,
     })
-
-    // res.json({
-    //   id,
-    //   location: {
-    //     lat,
-    //     lon,
-    //   },
-    //   weather: null,
-    // })
   }
 })
 
--- pages/sensors/[id].tsx
+++ pages/sensors/[id].tsx
@@ -1,61 +1,62 @@
-// import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from 'next'
-import { useRouter } from 'next/router'
-// import { sensorsList } from '../../data/sensorsList'
+import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from 'next'
+import { sensorsList } from '../../data/sensorsList'
 import styled from '@emotion/styled'
 
 type SensorDetailsPageProps = Readonly<{
-  details: Record<string, unknown> | null
-}>
+  id: string
+  data: unknown
+}> | null
 
 export default function SensorDetailsPage(props: SensorDetailsPageProps) {
-  const router = useRouter()
-  const { id } = router.query
+  if (props === null) {
+    return <p>Error</p>
+  }
 
   return (
     <SensorDetailsContainer>
-      <h2>Sensor {id}</h2>
+      <h2>Sensor {props.id}</h2>
       <pre>{JSON.stringify(props, null, 2)}</pre>
     </SensorDetailsContainer>
   )
 }
 
-// type StaticPath = Readonly<{
-//   id: string
-// }>
+type StaticPath = Readonly<{
+  id: string
+}>
 
-// export async function getStaticPaths(): Promise<GetStaticPathsResult<StaticPath>> {
-//   const paths = sensorsList.map((sensor) => ({
-//     params: {
-//       id: sensor.id.toString(), // must match filename param, e.g. [id].tsx
-//     },
-//   }))
+export function getStaticPaths(): GetStaticPathsResult<StaticPath> {
+  const paths = sensorsList.map((sensor) => ({
+    params: {
+      id: sensor.id.toString(), // must match filename param, e.g. [id].tsx
+    },
+  }))
 
-//   return {
-//     paths,
-//     fallback: false,
-//   }
-// }
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
-// export async function getStaticProps({
-//   params,
-// }: GetStaticPropsContext<StaticPath>): Promise<GetStaticPropsResult<SensorDetailsPageProps>> {
-//   if (params === undefined) {
-//     return {
-//       props: {
-//         details: null,
-//       },
-//     }
-//   }
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<StaticPath>): Promise<GetStaticPropsResult<SensorDetailsPageProps>> {
+  if (!params || !params.id) {
+    return {
+      props: null,
+    }
+  }
 
-//   const sensorDetailsResponse = await fetch(`${process.env.API_URL}/api/sensors/${params.id}`)
-//   const json = await sensorDetailsResponse.json()
+  const { id } = params
+  const response = await fetch(`${process.env.API_URL}/api/sensors/${id}`)
+  const data = await response.json()
 
-//   return {
-//     props: {
-//       details: json,
-//     },
-//   }
-// }
+  return {
+    props: {
+      id,
+      data,
+    },
+  }
+}
 
 const SensorDetailsContainer = styled.div`
   background-color: aliceblue;
